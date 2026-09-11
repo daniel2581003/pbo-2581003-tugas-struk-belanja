3f3ea361cde86d8fd5b75eddbdc246881e179fa9
Daniel Lim <2581003@unai.edu>
2026-09-11 10:01:14 +0700

    Barang 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nama Barang ke-1: ");
+        String nama1 = scanner.nextLine();
+
+        System.out.println("Harga Satuan: ");
+        double harga1 = scanner.nextDouble();
+
+        System.out.println("Jumlah: ");
+        int jumlah1 = scanner.nextInt();
+
+
+    }
+}
