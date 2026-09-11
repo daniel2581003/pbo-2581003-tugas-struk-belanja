c8342b7e97e5e406909772eb01c5319a36edc58f
Daniel Lim <2581003@unai.edu>
2026-09-11 10:11:13 +0700

    Uang dibayar

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,21 @@ public class Main {
         System.out.print("Jumlah: ");
         int jumlah2 = scanner.nextInt();
 
+        scanner.nextLine();
+
+        System.out.print("Nama Barang ke-3: ");
+        String nama3 = scanner.nextLine();
+
+        System.out.print("Harga Satuan: ");
+        double harga3 = scanner.nextDouble();
+
+        System.out.print("Jumlah: ");
+        int jumlah3 = scanner.nextInt();
+
+        System.out.print("Uang yg dibayar: ");
+        int uang = scanner.nextInt();
+
+
 
     }
 }
